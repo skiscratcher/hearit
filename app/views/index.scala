69d915d5b63b5d97fef5d70542635243263d2069
Arielle F <bb8andr2d2forlife@gmail.com>
2022-09-25 20:14:47 -0400

    Styles

--- app/views/index.scala
+++ app/views/index.scala
@@ -4,6 +4,7 @@ import scalatags.Text.all._
 object index {
   def main() = html(
     head(
+      link(href:="/assets/stylesheets/main.css", rel:="stylesheet")(),
       tag("title")("Scalatags testing")
     ),
     body(
